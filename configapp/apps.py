9f36a6b924e48d1e66949b1fa04e78c3e8078cf7
suxrob4ik77 <130899403+suxrob4ik77@users.noreply.github.com>
2025-02-09 20:38:34 +0500

    Delete configapp directory

--- configapp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ConfigappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'configapp'
--- configapp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path,include
-from project2.views import *
-
-# urlpatterns = [
-#     path('exe/', exe)
-#
-# ]
